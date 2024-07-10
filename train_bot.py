f48a3eff023b0f76db170ec4757e0a39e340ada0
isaacDR45 <isaacvargasvenegas035@gmail.com>
2024-07-10 17:40:18 -0500

    Add files via upload

---
+++ train_bot.py
@@ -0,0 +1,14 @@
+# Bibliotecas de preprocesamiento de datos de texto
+
+
+# Función para añadir palabras raíz (steam words)
+
+
+
+    
+        # Agregar todas las palabras de patrones a una lista
+        
+        # Agregar todas las etiquetas a la lista de clases
+         
+
+# Crear un corpus de palabras para el chatbot
